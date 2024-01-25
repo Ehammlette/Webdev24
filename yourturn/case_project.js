de365d5a64b7f3089b894e01133e0804ef23b23b
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-01-25 09:16:57 -0600

    Create case_project.js

---
+++ yourturn/case_project.js
@@ -0,0 +1,15 @@
+/*  JavaScript 7th Edition
+    Chapter 1
+    Hands-On Project 1-4
+
+    Author: Nora(Ethan) Hammons
+    Date:   1/23/2024
+
+    Filename: project.js
+*/
+showMeg = 1;
+
+window.onload= myfunction()
+{
+    showMeg
+}
