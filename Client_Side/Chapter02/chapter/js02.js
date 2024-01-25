f0affed4acda4620c6dc313c1124b85ec9d6d5bd
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-01-25 08:57:49 -0600

    Update js02.js

--- Client_Side/Chapter02/chapter/js02.js
+++ Client_Side/Chapter02/chapter/js02.js
@@ -4,9 +4,21 @@
 
       Fan Trick Fine Art Photography
       Variables and functions
-      Author: 
-      Date:   
+      Author: Nora(ethan) Hammons
+      Date:   1/25/2024
 
       Filename: js02.js
  */
+//set the form's default values
+window.addEventListener("load",setupForm);
+
+function setupForm()
+{
+document.getElementById("photoNum").value=1;
+document.getElementById("photoHrs").value=2;
+document.getElementById("makeBook").checked=false;
+document.getElementById("photoRights").checked=false;
+document.getElementById("photoDist").value=0;
+}
+
 
