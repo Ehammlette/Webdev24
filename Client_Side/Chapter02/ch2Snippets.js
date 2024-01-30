43784cfacdc09efd65873c15a4036997c1352c87
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-01-30 08:26:09 -0600

    Update ch2Snippets.js

--- Client_Side/Chapter02/ch2Snippets.js
+++ Client_Side/Chapter02/ch2Snippets.js
@@ -2,7 +2,11 @@ let user = "Ryan"; // global scope - global variable
 function showUser() 
 {
     let user ="Reynolds"; //local scope - local variable
+    console.log("In showUser - local scope");
     console.log(user);
 }
 
+console.log("Global scope")
 console.log(user); // print global variable
+
+showUser();
