5e5ce279549736db2f00a1bc611523fd5703b3fc
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-01-30 09:17:58 -0600

    Update case_project.js

--- Client_Side/case_project02/case_project.js
+++ Client_Side/case_project02/case_project.js
@@ -7,18 +7,23 @@
     Filename: project.js
 */
 
-
+//let comboNum1 =6; test code
 let correct_answer=false;
 
 // start game function on start of game
 window.addEventListener("load",startGame1)
 
+//see if puzzle is solved
 function startGame1()
 {
-    if ("comboNum1"==7)
+    //"comboNum1"
+    if (comboNum1==7)
     {
         correct_answer=true;
+        //console.log(correct_answer); test in console
     }
-    else;
+    else
+    //console.log(correct_answer); test in console
 }
-console.log(correct_answer)
+
+startGame1();
