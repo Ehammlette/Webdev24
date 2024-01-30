7321fd61f295537f021e24afea62c2048b8f82ab
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-01-30 08:37:21 -0600

    Update js02.js

--- Client_Side/Chapter02/chapter/js02.js
+++ Client_Side/Chapter02/chapter/js02.js
@@ -5,10 +5,17 @@
       Fan Trick Fine Art Photography
       Variables and functions
       Author: Nora(ethan) Hammons
-      Date:   1/25/2024
+      Date:   1/30/2024
 
       Filename: js02.js
  */
+
+//declare global constants for the application
+const EMP_COST = 100; //const photographers per hour
+const BOOK_COST = 350; //const of memory book
+const RREPRO_COST = 1250; // cost of reproduction rights
+const TRAVEL_COST = 2; // cost of travel per mile
+
 //set the form's default values
 window.addEventListener("load",setupForm);
 
@@ -22,5 +29,3 @@ document.getElementById("photoDist").value=0;
 }
 
 System.out.print();
-
-
