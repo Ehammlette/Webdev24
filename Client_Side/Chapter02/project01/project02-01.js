24084bf036d9f5fc38120d459f82a562719ad848
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-04 11:54:28 -0600

    Update project02-01.js

--- Client_Side/Chapter02/project01/project02-01.js
+++ Client_Side/Chapter02/project01/project02-01.js
@@ -3,8 +3,8 @@
       Project 02-01
 
       Celsius <-> Farenheit Coverter
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   2/4/2024
 
       Filename: project02-01.js
  */
