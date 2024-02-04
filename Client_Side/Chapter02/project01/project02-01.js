6c5f870a290b69937ce273e362c4981471b6363f
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-04 12:44:55 -0600

    Update project02-01.js

--- Client_Side/Chapter02/project01/project02-01.js
+++ Client_Side/Chapter02/project01/project02-01.js
@@ -9,3 +9,21 @@
       Filename: project02-01.js
  */
 
+function FahrenheitToCelsius(degree) {
+      return (degree - 32) / 1.8;
+}
+document.getElementById("cValue").onchange = function () 
+{
+      var cDegree = document.getElementById("cValue").value;
+      document.getElementById("fValue").value = CelsiusToFahrenheit(cDegree);
+
+}
+
+function CelsiusToFahrenheit(degree) {
+      return degree * 1.8 + 32
+}
+document.getElementById("fValue").onchange = function()
+{
+      var fDegree = document.getElementById("fValue").value;
+      document.getElementById("cValue").value = FahrenheitToCelsius(fDegree);
+}
