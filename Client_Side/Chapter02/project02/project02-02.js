529955d47114dffe0e7cbbc1c00c8c960ffda7cc
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-04 13:00:56 -0600

    Update project02-02.js

--- Client_Side/Chapter02/project02/project02-02.js
+++ Client_Side/Chapter02/project02/project02-02.js
@@ -8,4 +8,11 @@
 
       Filename: project02-02.js
  */
+
+function verifyForm()
+{
+      let name= document.getElementById("name").value;
+      let email= document.getElementById("email").value;
+      let phone= document.getElementById("phone").value;
+}
  
