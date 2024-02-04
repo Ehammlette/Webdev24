3a87fc1f84e301641a9abcf016e9eedb631281c5
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-04 12:47:05 -0600

    Update project02-02_txt.js

--- Client_Side/Chapter02/project02/project02-02_txt.js
+++ Client_Side/Chapter02/project02/project02-02_txt.js
@@ -3,8 +3,8 @@
       Project 02-02
 
       Application to test for completed form
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   2/4/2024
 
       Filename: project02-02.js
  */
