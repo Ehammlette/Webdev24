35a24b4cf929f339e82445e5d302e230a2d758c2
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-04 12:48:02 -0600

    Create project02-02.js

---
+++ Client_Side/Chapter02/project02/project02-02.js
@@ -0,0 +1,11 @@
+/*    JavaScript 7th Edition
+      Chapter 2
+      Project 02-02
+
+      Application to test for completed form
+      Author: Nora Hammons
+      Date:   2/4/2024
+
+      Filename: project02-02.js
+ */
+ 
