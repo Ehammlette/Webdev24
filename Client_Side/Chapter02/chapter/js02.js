2bb04f52c720cdc1902636c91e83e9405b1f3309
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-08 22:50:17 -0600

    Update js02.js

--- Client_Side/Chapter02/chapter/js02.js
+++ Client_Side/Chapter02/chapter/js02.js
@@ -17,20 +17,44 @@ const RREPRO_COST = 1250; // cost of reproduction rights
 const TRAVEL_COST = 2; // cost of travel per mile
 
 //set the form's default values
-window.addEventListener("load",setupForm);
+window.addEventListener("load", setupForm);
 
-function setupForm()
-{
-document.getElementById("photoNum").value=1;
-document.getElementById("photoHrs").value=2;
-document.getElementById("makeBook").checked=false;
-document.getElementById("photoRights").checked=false;
-document.getElementById("photoDist").value=0;
+function setupForm() {
+      document.getElementById("photoNum").value = 1;
+      document.getElementById("photoHrs").value = 2;
+      document.getElementById("makeBook").checked = false;
+      document.getElementById("photoRights").checked = false;
+      document.getElementById("photoDist").value = 0;
+
+      getEstimate();
+
+      //add event handlers for each input control
+      document.getElementById("photoNum").onchange = getEstimate;
+      document.getElementById("photoHrs").onchange= getEstimate;
+      document.getElementById("makeBook").onchange = getEstimate;
+      document.getElementById("photoRights").onchange = getEstimate;
+      document.getElementById("photoDist").onchange = getEstimate;
 }
 
-function getEstimate()
-{
-      
+function getEstimate() {
+      let totalCost = 0;
+      let photographers = document.getElementById("photoNum").value;
+      let hours = document.getElementById("photoHrs").value;
+      let distance = document.getElementById("photoDist").value;
+      let buyBook = document.getElementById("makeBook").checked;
+      let buyRights = document.getElementById("photoRights").checked;
+
+      //add the cost of photographers for the hours covered
+      totalCost += photographers * hours * EMP_COST;
+
+      //add the cost of distance per photographer per mile
+      totalCost += photographers * distance * TRAVEL_COST;
+
+      totalCost += buyBook ? BOOK_COST : 0;
+
+      totalCost += buyRights ? REPRO_COST : 0;
+
+      document.getElementById("estimate").innerHTML = "$" + totalCost;
 }
 
-System.out.print();
+System.out.print(estimate);
