e23ec8456a2421d96d2d98ead3fa11e408d03696
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-15 10:04:43 -0600

    Update case_project.js

--- Client_Side/case_project02/case_project.js
+++ Client_Side/case_project02/case_project.js
@@ -6,12 +6,15 @@
 
     Filename: project.js
 */
+const MIN_NUMBER=1;
+const MAX_NUMBER= 1000;
 let totalCost=0;
 
 window.addEventListener("load", setupForm);
 
 function setupForm()
 {
+    document.getElementById("display").innerHTML=generateAnswer;
     document.getElementById("start_number").value=10;
     document.getElementById("range_input").value=50;
 
@@ -19,7 +22,7 @@ function setupForm()
 
     //add event handlers for each input control
     document.getElementById("start_number").onchange = getEstimate;
-    document.getElementById("range_input").onchange = getEstimate
+    document.getElementById("range_input").onchange = getEstimate;
 
 }
 
@@ -32,4 +35,14 @@ function getEstimate()
     totalCost+= number_input* slider_range;
 
     document.getElementById("estimate").innerHTML = totalCost;
+}
+
+function generateAnswer()
+{
+    return Math.random() * (MAX_NUMBER - MIN_NUMBER) + MIN_NUMBER;
+}
+
+function checkAnswer()
+{
+
 }
