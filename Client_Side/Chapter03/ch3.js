893fd8428232076e245fc16e34244a33bea347f4
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-20 08:47:16 -0600

    Create ch3.js

---
+++ Client_Side/Chapter03/ch3.js
@@ -0,0 +1,43 @@
+//Note: When empty in array the element is UNDEFINED not null
+
+//initializing array with values
+let month=["jan","Feb","Mar", "Apr","May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+
+//initializing array empty array
+let dataValue=[];
+
+//initializing array months2 using new Array
+let months2 = new Array("jan","Feb","Mar", "Apr","May","Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec");
+
+//initializing array
+let dayWeek = new Array(8);
+let monthName = new Array(12);
+
+//initializing array
+let vals=[,,,];
+console.log(vals[0]);
+vals[0] = "Eleanora";
+console.log(vals[0]);
+vals[4] = "johnson";
+console.log(vals[4]);
+console.log(vals); //display the contentss of the array iin the format of [value1,...,...,value4]
+console.log("The size of the array vals is " + vals.length); // get the size of the array
+
+//initialize days of the week
+daysWeek[0] = "";
+daysWeek[1] = "Monday";
+daysWeek[2] = "Tuesday";
+daysWeek[3] = "Wednesday";
+daysWeek[4] = "Thursday";
+daysWeek[5] = "Friday";
+daysWeek[6] = "Saturday";
+daysWeek[7] = "Sunday";
+
+//Sparse Array
+let sparse = new Array();
+sparse[0] = 1;
+sparse[99] = 100;
+
+console.log(sparse[0]);
+console.log(sparse[1]);
+console.log(sparse[99]);
