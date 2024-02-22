da8611aa7b716765444dcc9dc51b50bc441087a2
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-22 13:49:39 -0600

    Update js03.js

--- Client_Side/Chapter03/chapter/js03.js
+++ Client_Side/Chapter03/chapter/js03.js
@@ -13,3 +13,31 @@
      //Days of the Week
      let weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
 
+     window.addEventListener("load",showGames);
+     window.addEventListener("load", addWeekDays);
+
+     function showGames()
+     {
+        for (let i=0; i <gameDates.length; i++)
+        {
+            let gameInfo = "";
+
+            //Open the paragraph
+            gameInfo += "<p>";
+
+            //Include the opponent
+            gameInfo += gameOpponents[i] + "<br>";
+
+            //Include result and score
+            gameInfo += gameResults[i]+ ": (" + runsScored[i] + 
+            " - " + runsAllowed[i] + ")";
+
+            //Close the paragraph
+            gameInfo += "</p>";
+
+            //Write the information into a table cell 
+            let tableCell = document.getElementById(gameDates[i]);
+            tableCell.insertAdjacentHTML("beforeEnd", gameInfo);
+        }
+     }
+
