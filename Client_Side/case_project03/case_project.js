a5f1d31bfd85ba6e361219f36ab55b79439a2734
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-02-27 15:39:08 -0600

    Update case_project.js

--- Client_Side/case_project03/case_project.js
+++ Client_Side/case_project03/case_project.js
@@ -2,25 +2,22 @@
     case project
 
     Author: Nora(Ethan) Hammons
-    Date:   2/20/2024
+    Date:   2/27/2024
 
     Filename: project.js
 */
-const MIN_NUMBER=1;
-const MAX_NUMBER= 1000;
-let frequency=0;
+let frequency = 0;
 const element = document.getElementById("check_button");
 
 window.addEventListener("load", setupForm);
-element.addEventListener("click",checkAnswer);
+element.addEventListener("click", checkAnswer);
 
-function setupForm()
-{
-    document.getElementById("looking_for_frequency").innerHTML=;
-    document.getElementById("number1").value=10;
-    document.getElementById("range_input").value=50;
+function setupForm() {
+    document.getElementById("looking_for_frequency").innerHTML = choose_num();
+    document.getElementById("number1").value = 10;
+    document.getElementById("range_input").value = 50;
 
-
+    //Call to find frequency
     getCurrentFrequency();
 
     //add event handlers for each input control
@@ -29,31 +26,75 @@ function setupForm()
 
 }
 
-function getCurrentFrequency()
-{
-    frequency=0;
-    let channel=document.getElementById("number1").value;
-    let slider_range=document.getElementById("range_input").value;
+function getCurrentFrequency() {
+    let frequency = 0;
+    let channel = document.getElementById("number1").value;
+    let slider_range = document.getElementById("range_input").value;
 
     //displays the range input
-    document.getElementById("range_of_input").innerHTML=document.getElementById("range_input").value;
+    document.getElementById("range_of_input").innerHTML = document.getElementById("range_input").value;
 
-    //get get frequency
-    totalCost= channel + "." +slider_range;
-    
+    //get get frequency + trouble if statement to fix bug + number bug fix
+    if (slider_range === 100) {
+        frequency = channel + 1;
+    }
+    else {
+        if (slider_range < 10) {
+            frequency = channel + "." + (slider_range / 100).toPrecision(1);
+        }
+        else {
+            frequency = channel + "." + (slider_range / 100).toPrecision(2);
+        }
+    }
+
 
     //type total into span currentFrequency
-    document.getElementById("currentFrequency").innerHTML = totalCost;
+    document.getElementById("currentFrequency").innerHTML = frequency;
 }
 
-function checkAnswer()
-{
+function checkAnswer() {
     document.getElementById("test").innerHTML = "Hello World";
-return 0;
+    return 0;
 }
 
 //chooses a random number that is set to be a specific 4 digit integer
-function choose_num()
-{
-    Math.floor(Math.random() * 10)+ "." +(Math.floor(Math.random() * 100))/100
+function choose_num() {
+    let chosen_num = Math.floor(Math.random() * 12);
+    //let chosen_freq=(Math.floor(Math.random() * 101));
+    let chosen_freq = 5;
+
+    if (chosen_freq == 0 && chosen_num != 11) {
+        chosen_num = chosen_num + .00;
+    }
+    else if (chosen_freq === 100 && chosen_num != 11) {
+        chosen_num = chosen_num + 1;
+    }
+    else if (chosen_num === 11) {
+        chosen_num = 11;
+    }
+    else {
+        if (chosen_freq < 10)
+        {
+            chosen_num = chosen_num + (chosen_freq / 100).toPrecision(1);  
+        }
+        else 
+        {
+            chosen_num = chosen_num + (chosen_freq / 100).toPrecision(2);
+        }
+    }
+    /*
+    let chosen_num=0;
+    let chosen_channel= Math.floor(Math.random() * 10);
+    let chosen_freq= Math.floor(Math.random() * 100);
+
+    if (chosen_freq<100)
+    {
+        chosen
+    }
+    else if (chosen_freq===100)
+    {
+        chosen_num = chosen_channel +
+    }
+    */
+    return chosen_num;
 }
