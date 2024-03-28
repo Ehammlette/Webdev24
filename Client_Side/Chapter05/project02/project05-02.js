495c0eb188344e109263fc4798c3c0e56a324e02
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-03-28 11:26:13 -0500

    Update project05-02.js

--- Client_Side/Chapter05/project02/project05-02.js
+++ Client_Side/Chapter05/project02/project05-02.js
@@ -5,7 +5,7 @@
 
       Project to move images between a photo bucket and photo list.
       Author: Nora Hammons
-      Date:   3/21/2024
+      Date:   3/28/2024
 
       Filename: project05-02.js
 */
@@ -19,3 +19,32 @@ let photoBucket = document.getElementById("photo_bucket");
 //The photoList variable referencing the element with the id “photo_list”
 let photoList = document.getElementById("photo_list");
 
+for (var i=0; i< images.length; i++)
+{
+      //incites images
+      var image =images[i];
+
+      //add click event listener to each image
+      image.addEventListener("click", function(){
+            //check if parent element of the clicked image has the same id as photo_bucket
+            if(this.parentElement.id=== "photo_bucket")
+            {
+                  //if true, create a new list item element
+                  var newItem = document.createElement("li");
+                  //Append the clicked image to the new list item
+                  newItem.appendChild(this);
+                  //append the new list item to the photoList
+                  photoList.appendChild(newItem);
+            }
+            else {
+                  // If the parent element is not photo_bucket
+                  // Declare a variable named oldItem equal to the parent element of the clicked image
+                  var oldItem = this.parentElement;
+                  //Append the clicked image back to the photoBucket
+                  photoBucket.appendChild(this);
+                  //Remove old Item form it's parent element
+                  oldItem.parentElement.removeChild(oldItem);
+            }
+      });
+
+}
