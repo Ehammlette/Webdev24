c3db02fe4a6de01dbf9e5a87963c78b0c2a953f3
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-01 10:11:35 -0500

    Update project06-01.js

--- Client_Side/Chapter06/project01/project06-01.js
+++ Client_Side/Chapter06/project01/project06-01.js
@@ -18,11 +18,11 @@
  let pwd2 = document.getElementById("pwd2");
 
 submitButton.addEventListener("click", function() {
-      if(pwd.length < 8 || /^[A-Za-z0-9]*$/g.test(pwd.value)===false)
+      if(pwd.value.length < 8 || /^[A-Za-z0-9]*$/g.test(pwd.value)===false)
       {
             pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number.");
       }
-      else if(pwd.value != pwd2.value && pwd.length >= 8)
+      else if(pwd.value != pwd2.value && /^[A-Za-z0-9]*$/.test(pwd.value) && pwd.value.length >= 8)
       {
             pwd2.setCustomValidity("Your passwords must match");
       }
