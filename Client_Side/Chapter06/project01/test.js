2f86193b0d628cf1cbd25cb954a36d582932be14
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-01 08:25:40 -0500

    Create test.js

---
+++ Client_Side/Chapter06/project01/test.js
@@ -0,0 +1,15 @@
+let pwd="1kpd2lDr";
+let pwd2="1kpd2lDr";
+
+if(pwd.length < 8)
+{
+    console.log("first if")
+}
+else if(pwd != pwd2)
+{
+    console.log("second if")
+}
+else
+{
+    console.log("third if")
+}
