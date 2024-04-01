d503a9fb2725d74d5455dfd220d12dd8046a7da6
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-01 08:45:49 -0500

    Update test.js

--- Client_Side/Chapter06/project01/test.js
+++ Client_Side/Chapter06/project01/test.js
@@ -1,11 +1,11 @@
 let pwd="1kpd2lDr";
 let pwd2="1kpd2lDr";
 
-if(pwd.length < 8)
+if(pwd.length < 8 || /^[A-Za-z0-9]*$/.test(pwd)===false)
 {
     console.log("first if")
 }
-else if(pwd != pwd2)
+else if(pwd != pwd2 && /^[A-Za-z0-9]*$/.test(pwd) && pwd.length >= 8)
 {
     console.log("second if")
 }
