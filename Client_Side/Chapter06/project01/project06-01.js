2932b66bc21276c4819d6e9f27adde400bd3d137
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-01 08:25:34 -0500

    Update project06-01.js

--- Client_Side/Chapter06/project01/project06-01.js
+++ Client_Side/Chapter06/project01/project06-01.js
@@ -10,3 +10,21 @@
       Filename: project06-01.js
 */
 
+ //declaring submit button is referencing Element submitButton
+ let submitButton = document.getElementById("submitButton");
+//declaring pwd is referencing Element pwd
+ let pwd = document.getElementById("pwd");
+ //declaring pwd2 is referencing Element pwd2
+ let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function() {
+if(pwd.length < 8)
+{
+
+}
+else if(pwd != pwd2)
+{
+
+}
+else
+});
