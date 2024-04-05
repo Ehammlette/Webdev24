a45e6bc72ef804fecc52f5dcb798d1157ad0cc74
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-05 14:33:05 -0500

    Update case_project.js

--- Client_Side/case_project06/case_project.js
+++ Client_Side/case_project06/case_project.js
@@ -138,5 +138,26 @@ function restartGame()
     location.reload();
 }
 
-/*--------security_page--------*/
+/*--------UserPassLogic.js--------*/
 
+ //declaring submit button is referencing Element submitButton
+ let submitButton = document.getElementById("submitButton");
+//declaring pwd is referencing Element pwd
+ let pwd = document.getElementById("pwd");
+ //declaring pwd2 is referencing Element pwd2
+ let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function() {
+      if(pwd.value.length < 8 || /^[A-Za-z0-9]*$/g.test(pwd.value)===false)//if password is less then 8 letters/numbers or doesn't have a letter and a number
+      {
+            pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number.");
+      }
+      else if(pwd.value != pwd2.value && /^[A-Za-z0-9]*$/.test(pwd.value) && pwd.value.length >= 8)// if passwords aren't matching
+      {
+            pwd2.setCustomValidity("Your passwords must match");
+      }
+      else
+      {
+          pwd.setCustomValidity("");
+      }
+});
