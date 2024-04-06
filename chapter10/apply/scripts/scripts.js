dcba56ccfb2eb81d2ed3260d105884d7e864e5ec
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-06 10:43:09 -0500

    Update scripts.js

--- chapter10/apply/scripts/scripts.js
+++ chapter10/apply/scripts/scripts.js
@@ -3,3 +3,11 @@
 	File Name: 
 	Current Date: 4/5/2024
 */
+
+/*Function to display content*/
+function content(){
+	var text = document.getElementById("new");
+	text.textContent= "Hi. I am a new paragraph that a JavaScript function.";
+	text.style.color = "#c0145f";
+	text.style.fontSize = "2em";
+}
