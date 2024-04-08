a9c1b4a85356f971b1489c81e5f4451c1f8c3834
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-08 09:49:24 -0500

    Update script.js

--- chapter10/extend/scripts/script.js
+++ chapter10/extend/scripts/script.js
@@ -3,3 +3,12 @@
     File Name: script.js
     Date: 4/8/2024
 */
+
+
+$("#hide").click(function(){
+    $("article p").hide();
+  });
+  
+  $("#show").click(function(){
+    $("article p").show();
+  });
