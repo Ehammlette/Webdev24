5f01b0d78d71d8f18d64a01b6556f17c3639b326
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-08 09:53:43 -0500

    Update script.js

--- chapter10/analyze/scripts/script.js
+++ chapter10/analyze/scripts/script.js
@@ -1,12 +1,12 @@
 /*
-    Student Name:
+    Student Name: Nora Hammons
     File Name: script.js
     Date: 
 */
 
 //Function to display the first picture
-function pic1 {
+function pic1() {
     imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
-    figCap.textContent = "Trunk Bay in St. John";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
+    figCap.textContent = "Trunk Bay in St. John";}
