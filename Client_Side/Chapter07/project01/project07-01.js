3b633bf34dc0d955acaefe76fe1bd6d52cfed2a8
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-11 08:13:24 -0500

    Update project07-01.js

--- Client_Side/Chapter07/project01/project07-01.js
+++ Client_Side/Chapter07/project01/project07-01.js
@@ -4,8 +4,8 @@
       Project 07-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   4/11/2024
 
       Filename: project07-01.js
 */
