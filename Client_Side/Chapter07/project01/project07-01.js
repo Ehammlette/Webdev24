87743231b5f60fbe7a5ee2cda705a51a1cf47a5c
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-11 08:58:44 -0500

    Update project07-01.js

--- Client_Side/Chapter07/project01/project07-01.js
+++ Client_Side/Chapter07/project01/project07-01.js
@@ -10,15 +10,16 @@
       Filename: project07-01.js
 */
 
+var regex1= /[A-Z]/;
+var regex2= /[0-9]/;
+var regex3= /[!$#%.]/;
+
 let signupForm = document.getElementById("signup");
 
 signupForm.addEventListener("submit", function(e) 
 { 
    let pwd = document.getElementById("pwd").value;
    let feedback = document.getElementById("feedback");
-   var regex1= /[A-Z]/;
-   var regex2= /[0-9]/;
-   var regex3= /[!\$#%.]/;
 
    e.preventDefault();
 
@@ -26,21 +27,21 @@ signupForm.addEventListener("submit", function(e)
    {
       feedback.textContent= "Your password must be at least 8 characters.";
    }
-   else if(regex1.test(pwd))
+   else if(!regex1.test(pwd))
    {
       feedback.textContent= "Your password must include an uppercase letter.";
    }
-   else if(regex2.test(pwd))
+   else if(!regex2.test(pwd))
    {
       feedback.textContent = "Your password must include a number.";
    }
-   else if (regex3.test(pwd))
+   else if (!regex3.test(pwd))
    {
       feedback.textContent= "Your password must include one of the following: !$#%";
    }
    else
    {
-      sub
+      signupForm.submit();
    }
 
 }
