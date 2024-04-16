e59320bdee470730230bd9ca7d32cfe170152227
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-16 10:00:51 -0500

    Update manual.js

--- Client_Side/case_project07/manual.js
+++ Client_Side/case_project07/manual.js
@@ -7,3 +7,35 @@
     Filename: manual.js
 */
 
+// generates host array to be paired with a destination
+let hosts= new Array(10);
+hosts[0]="Zenithia";
+hosts[1]="Hypernova";
+hosts[2]="Cygnion";
+hosts[3]="Andromax";
+hosts[4]="Orionis";
+hosts[5]="Vegalum";
+hosts[6]="Novaria";
+hosts[7]="Dracoria";
+hosts[8]="Phoenixa";
+hosts[9]="Nebsol";
+
+
+// generates destinations array to be paired with a host
+let destinations = new Array(10);
+destinations[0]="";
+destinations[1]="";
+destinations[2]="";
+destinations[3]="";
+destinations[4]="";
+destinations[5]="";
+destinations[6]="";
+destinations[7]="";
+destinations[8]="";
+destinations[9]="";
+
+// generates corresponding to host array to be paired with a destination
+let code_hosts= new Array(10);
+
+// generates code corresponding to destinations array to be paired with a host
+let code_destinations = new Array(10);
