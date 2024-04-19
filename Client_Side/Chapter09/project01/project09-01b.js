0e3754caf744e0577dc10ed722cda730bba52537
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-19 09:49:50 -0500

    Update project09-01b.js

--- Client_Side/Chapter09/project01/project09-01b.js
+++ Client_Side/Chapter09/project01/project09-01b.js
@@ -4,9 +4,11 @@
       Project 09-01
 
       Project to read field values from a query string
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   4/19/2024
 
       Filename: project09-01b.js
 */
+
+
 
