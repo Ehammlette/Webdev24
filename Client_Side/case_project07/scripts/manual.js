8b8d43626988086ebf3a355cdbd4c4babf733b04
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-18 20:39:18 -0500

    Update manual.js

--- Client_Side/case_project07/scripts/manual.js
+++ Client_Side/case_project07/scripts/manual.js
@@ -10,68 +10,68 @@
 /*--------Arrays--------*/
 
 // generates host array to be paired with a destination
-let hosts= new Array(10);
-hosts[0]="Zenithia";
-hosts[1]="Hypernova";
-hosts[2]="Cygnion";
-hosts[3]="Andromax";
-hosts[4]="Orionis";
-hosts[5]="Vegalum";
-hosts[6]="Novaria";
-hosts[7]="Dracoria";
-hosts[8]="Phoenixa";
-hosts[9]="Nebsol";
-
-// generates destinations array to be paired with a host
-let destinations = new Array(10);
-destinations[0]="Lumisynth";
-destinations[1]="Galactron";
-destinations[2]="Quasara";
-destinations[3]="Chronium";
-destinations[4]="Spectra";
-destinations[5]="Titanor";
-destinations[6]="Zephyria";
-destinations[7]="Exolux";
-destinations[8]="Nebulaeum";
-destinations[9]="Aetherium";
+let satellites= new Array(20);
+satellites[0]="Zenithia";
+satellites[1]="Hypernova";
+satellites[2]="Cygnion";
+satellites[3]="Andromax";
+satellites[4]="Orionis";
+satellites[5]="Vegalum";
+satellites[6]="Novaria";
+satellites[7]="Dracoria";
+satellites[8]="Phoenixa";
+satellites[9]="Nebsol";
+satellites[10]="Lumisynth";
+satellites[11]="Galactron";
+satellites[12]="Quasara";
+satellites[13]="Chronium";
+satellites[14]="Spectra";
+satellites[15]="Titanor";
+satellites[16]="Zephyria";
+satellites[17]="Exolux";
+satellites[18]="Nebulaeum";
+satellites[19]="Aetherium";
 
 // generates corresponding to host array to be paired with a destination
-let code_hosts= new Array(10);
-code_hosts[0]="417";
-code_hosts[1]="839";
-code_hosts[2]="562";
-code_hosts[3]="104";
-code_hosts[4]="725";
-code_hosts[5]="326";
-code_hosts[6]="981";
-code_hosts[7]="243";
-code_hosts[8]="678";
-code_hosts[9]="540";
+let code_satellites= new Array(20);
+code_satellites[0]="417";
+code_satellites[1]="839";
+code_satellites[2]="562";
+code_satellites[3]="104";
+code_satellites[4]="725";
+code_satellites[5]="326";
+code_satellites[6]="981";
+code_satellites[7]="243";
+code_satellites[8]="678";
+code_satellites[9]="540";
+code_satellites[10]="129";
+code_satellites[11]="573";
+code_satellites[12]="849";
+code_satellites[13]="306";
+code_satellites[14]="457";
+code_satellites[15]="628";
+code_satellites[16]="715";
+code_satellites[17]="934";
+code_satellites[18]="281";
+code_satellites[19]="569";
 
-// generates code corresponding to destinations array to be paired with a host
-let code_destinations = new Array(10);
-code_destinations[0]="129";
-code_destinations[1]="573";
-code_destinations[2]="849";
-code_destinations[3]="306";
-code_destinations[4]="457";
-code_destinations[5]="628";
-code_destinations[6]="715";
-code_destinations[7]="934";
-code_destinations[8]="281";
-code_destinations[9]="569";
+/*--------End Arrays--------*/
 
-/*-------- End Arrays--------*/
 
-window.addEventListener("load", function(){
+/*-------- --------*/
+window.addEventListener("load",function(){
     let htmlCode="";
 
-    for(let i=0; i<hosts.length;i++)
+
+
+    for(let i=0; i<satellites.length;i++)
         {
-                htmlCode+='<p>'+(i+1)+'. '+ hosts[i]+
+                htmlCode+='<p>'+(i+1)+'. '+ satellites[i]+': '+ code_satellites[i]+
                 '</p>';
         }
 
     //put the displayed code into div element
     document.getElementById("listOfThings").innerHTML=htmlCode;
 });
+
+/*--------Dropdown--------*/
