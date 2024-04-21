ff4a9554d66004cdf5bcb17f89ef187a7144ab7b
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-20 19:47:38 -0500

    Update form.js

--- Client_Side/case_project07/scripts/form.js
+++ Client_Side/case_project07/scripts/form.js
@@ -17,17 +17,21 @@
  let pwd2 = document.getElementById("pwd2");
 
 submitButton.addEventListener("click", function() {
-    if(/^[A-Za-z0-9]*$/.test(pwd.value)===false)
+    if(/^[A-Za-z0-9]/g.test(pwd.value)===false)//doesn't have a letter and a number
     {
         pwd.setCustomValidity("Your password must have at least one letter and one number.");
     }
-      else if(pwd.value.length < 8 )//if password is less then 8 letters/numbers or doesn't have a letter and a number
+      else if(pwd.value.length < 8 )//if password is less then 8 letters/numbers
       {
             pwd.setCustomValidity("Your password must be at least 8 characters.");
       }
       else if(pwd.value != pwd2.value && pwd.value.length >= 8)// if passwords aren't matching
       {
             pwd2.setCustomValidity("Your passwords must match");
+      }
+      else if (/[\W]/g.test(pwd.value))
+      {
+        pwd.setCustomValidity("Don't use special characters");
       }
       else
       {
