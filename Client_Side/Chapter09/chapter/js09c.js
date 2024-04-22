0dcfeb1fa0bcdc3b1b7b1d2bfa2a0ad26bc12545
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-22 14:39:05 -0500

    Update js09c.js

--- Client_Side/Chapter09/chapter/js09c.js
+++ Client_Side/Chapter09/chapter/js09c.js
@@ -10,4 +10,22 @@
       Filename: js09c.js
  */
 
-      //Eating 
+      //Eating Well Preference Keys
+      let keys= ["name", "email", "phone", "address", "city", "state", "zip", "allergies", "frequency", "size"];
+
+      for (let item of keys) {
+          let newRow = document.createElement("tr");
+
+          //Display the storage key
+          let keyCell= document.createElement("td");
+          keyCell.textContent = item;
+          newRow.appendChild(keyCell);
+
+          //Display the key value
+          let keyValue = document.createElement("td");
+          keyValue.textContent= localStorage.getItem(item);
+          newRow.appendChild(keyValue);
+
+          //Append each key=name pair as a table row
+          document.getElementById("prefTable").appendChild(newRow);
+      }
