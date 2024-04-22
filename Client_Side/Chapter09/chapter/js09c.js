630763d163bac8224797d2310c5529479bb6fd64
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-22 14:26:43 -0500

    Update js09c.js

--- Client_Side/Chapter09/chapter/js09c.js
+++ Client_Side/Chapter09/chapter/js09c.js
@@ -4,8 +4,8 @@
       Chapter case
       
       Eating Well in Season Retrieving Data from Local Storage
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date: 4/22/2023
       
       Filename: js09c.js
  */
