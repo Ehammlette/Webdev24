3964c2e59a0591b54e34ff67c4362850e7a6d7e0
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-22 16:40:47 -0500

    Update project09-03.js

--- Client_Side/Chapter09/project03/project09-03.js
+++ Client_Side/Chapter09/project03/project09-03.js
@@ -4,8 +4,8 @@
       Project 09-03
 
       Project to retrieve date of last visit from web storage and mark new article
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   4/22/2024
 
       Filename: project09-03.js
 */
