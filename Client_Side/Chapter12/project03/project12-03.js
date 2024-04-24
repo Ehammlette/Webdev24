fdaead97b31660f8939ca616cbb6c29c4f6e8b37
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-24 10:25:24 -0500

    Update project12-03.js

--- Client_Side/Chapter12/project03/project12-03.js
+++ Client_Side/Chapter12/project03/project12-03.js
@@ -13,4 +13,29 @@
 
 document.querySelector('article > h2').addEventListener("click",function(){
 
+      var heading= event.target;
+
+      var list= heading.nextElementSibling;
+
+      var headingImage= heading.querySelector('img');
+
+      //Toggel the list using slideToggle
+      let isVisible = true;
+      setInterval(() => {
+            if (isVisible){
+                  list.style.display = 'none';
+            }
+            else{
+                  list.style.display='block';
+            }
+            isVisible = !isVisible;
+      },500);
+
+      const src = headingImage.getAttribute('src');
+      if (src === 'plus.png') {
+        headingImage.setAttribute('src', 'minus.png');
+      } else {
+        headingImage.setAttribute('src', 'plus.png');
+      }
+
 });
