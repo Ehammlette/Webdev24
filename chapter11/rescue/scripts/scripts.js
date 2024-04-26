62885c2a28ada391327fcd31d9d204683bfd8276
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-26 16:40:53 -0500

    Update scripts.js

--- chapter11/rescue/scripts/scripts.js
+++ chapter11/rescue/scripts/scripts.js
@@ -18,13 +18,13 @@ function hamburger(){
     }
     else {
         navlinks.style.display = "block";
-        if(menuicon.style.color === "#f6eee4")
+        if(menuicon.style.color === "#2alf14" )
+        {
+            menuicon.style.color = "#f6eee4"
+        }
+        else
         {
             menuicon.style.color = "#2alf14";
-        }
-        else(menuicon.style.color === "#2alf14")
-        {
-            menuicon.style.color = "#f6eee4";
         }
     }
 }
