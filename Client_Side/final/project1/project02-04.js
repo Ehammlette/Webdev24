fa842e7d6a85df992ec7bd7bf2ec4d536aa28ef9
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-30 08:44:25 -0500

    Update project02-04.js

--- Client_Side/final/project1/project02-04.js
+++ Client_Side/final/project1/project02-04.js
@@ -15,11 +15,23 @@
    let SALAD_PRICE = 7.95
    let SALES_TAX = 0.07
 
+   window.addEventListener("load", test);
+
+   function test(){
+      calcTotal();
+
+      document.getElementById("number1").onchange=calcTotal;
+   }
+
 
 //calculate cost
 function calcTotal(){
    let cost=0;
-   var buyChicken = document.getElementById("chicken").ariaChecked;
+   var buyChicken = document.getElementById("chicken").checked;
+   if (buyChicken)
+   {
+      document.getElementById("totalBill")=buyChicken;
+   }
 
 }
 
