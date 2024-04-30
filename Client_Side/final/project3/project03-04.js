4632f17d3fdc1ab1699ec15ef2234c445b7a90e5
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-30 09:55:06 -0500

    Update project03-04.js

--- Client_Side/final/project3/project03-04.js
+++ Client_Side/final/project3/project03-04.js
@@ -3,8 +3,8 @@
       Project 03-04
 
       Application to write a list of customer reviews
-      Author: 
-      Date:   
+      Author: Nora Hammons
+      Date:   4/29/2024
 
       Filename: project03-04.js
 */
@@ -21,3 +21,43 @@ let reviews = [
 ];
 let reviewTitles = ["My Favorite Workout Game", "Poor Choreography", "Buggy with Poor Tech Support", "Nice Improvement"];
 
+//starImages
+function starImages(rating) {
+      // Declare a variable named imageText, setting its initial value to an empty text string
+      let imageText = '';
+  
+      // Create a for loop to generate star images based on the rating parameter
+      for (let i = 1; i <= rating; i++) {
+          // Add star image tags to the imageText variable
+          imageText += "<img src='star.png' alt=''>";
+      }
+  
+      // Return the value of imageText from the function
+      return imageText;
+  }
+  
+  // Assuming the reviewers array is defined elsewhere in your code
+  for (let i = 0; i < reviewers.length; i++) {
+      let reviewCode = '';
+  
+      // Add table opening tag based on reviewType
+      if (reviewType[i] === "P") {
+          reviewCode += "<table class='prime'>";
+      } else if (reviewType[i] === "N") {
+          reviewCode += "<table class='new'>";
+      } else {
+          reviewCode += "<table>";
+      }
+  
+      // Add HTML code for table rows and data cells
+      reviewCode += "<tr><th>Review Titles[" + i + "]</th><td>" + reviewers[i] + "</td></tr>";
+      reviewCode += "<tr><th>Review Date</th><td>" + reviewDates[i] + "</td></tr>";
+      reviewCode += "<tr><td colspan='2'>" + reviews[i] + "</td></tr>";
+  
+      // Add table closing tag
+      reviewCode += "</table>";
+  
+      // Insert the value of the reviewCode variable into the first <article> tag in the document
+      document.getElementsByTagName('article')[0].insertAdjacentHTML('beforeend', reviewCode);
+  }
+  
