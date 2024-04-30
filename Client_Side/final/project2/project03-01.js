9eb6eaf1bcdcfd9c3ce9dfc8d143ba7844770b77
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-04-30 09:36:43 -0500

    Update project03-01.js

--- Client_Side/final/project2/project03-01.js
+++ Client_Side/final/project2/project03-01.js
@@ -9,9 +9,25 @@
       Filename: project03-01.js
 */
 
+//declare menuItems
+let menuItems=getElementsByClassName(menuItem);
 
+//for loop
+for(let i=0; i<menuItems.length; i++){
+      menuItems[i].addEventListener('click', calcTotal);
+}
 
+document.getElementById("billTotal").innerHTML=formatCurrency(orderTotal);
 
+//function calcTotal
+function calcTotal(){
+      let orderTotal=0;
+
+      if (menuItems[i].checked) {
+            // Increase orderTotal by the value of menuItems[i]
+            orderTotal += Number(menuItems[i].value);
+        }
+}
 
  // Function to display a numeric value as a text string in the format $##.## 
  function formatCurrency(value) {
