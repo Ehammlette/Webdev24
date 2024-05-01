5880c31d3a27593fa071ac0dd0b26160bfe262b0
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-05-01 12:00:07 -0500

    Update lightbox_data.js

--- Final/lightbox/lightbox_data.js
+++ Final/lightbox/lightbox_data.js
@@ -12,12 +12,10 @@
 let lightboxTitle = "ShowCase";
 
 // Names of the image files shown in the slideshow
-let imgFiles = ["photo01.jpg", "photo02.jpg", "photo03.jpg", "photo04.jpg",
-                "photo05.jpg", "photo06.jpg", "photo07.jpg", "photo08.jpg",
-                "photo09.jpg", "photo10.jpg", "photo11.jpg", "photo12.jpg"]
+let imgFiles = ["image/screenshot/Chapter2_temp.jpg","image/screenshot/Chapter3_analyze.jpg","image/screenshot/Chapter4_analyze.jpg","image/screenshot/Chapter5_extend.jpg","image/screenshot/Chapter6_extend.jpg","image/screenshot/Chapter7_analyze.jpg","image/screenshot/Chapter9_apply.jpg","image/screenshot/Chapter12_apply.jpg"]
 
 // Captions associated with each image
-let imgCaptions = new Array(12);
+let imgCaptions = new Array(8);
 imgCaptions[0]="Sky Pond (Rocky Mountain National Park)";
 imgCaptions[1]="Buffalo on the Plains (South Dakota)"; 
 imgCaptions[2]="Garden of the Gods (Colorado Springs)"; 
@@ -26,10 +24,6 @@ imgCaptions[4]="Double Rainbow (Colorado National Monument)";
 imgCaptions[5]="Moose in the Wild (Grand Lake, Colorado)";
 imgCaptions[6]="Camas Wild Flower (Rocky Mountain National Park)";
 imgCaptions[7]="Chasm Lake (Rocky Mountain National Park)";
-imgCaptions[8]="Teton Crest Trail (Grand Teton National Park)";
-imgCaptions[9]="The Notch Trail (Badlands National Park)";
-imgCaptions[10]="Sprague Lake (Rocky Mountain National Park)";
-imgCaptions[11]="Longs Peak Trail (Rocky Mountain National Park)";
 
 // Count of images in the slideshow
 let imgCount = imgFiles.length;
