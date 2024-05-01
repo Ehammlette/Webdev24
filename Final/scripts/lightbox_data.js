d5036a002bb8b2d106f1952583a020198eda3295
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-05-01 14:01:07 -0500

    Update lightbox_data.js

--- Final/scripts/lightbox_data.js
+++ Final/scripts/lightbox_data.js
@@ -14,16 +14,18 @@ let lightboxTitle = "ShowCase";
 // Names of the image files shown in the slideshow
 let imgFiles = ["image/screenshot/Chapter2_temp.jpg","image/screenshot/Chapter3_analyze.jpg","image/screenshot/Chapter4_analyze.jpg","image/screenshot/Chapter5_extend.jpg","image/screenshot/Chapter6_extend.jpg","image/screenshot/Chapter7_analyze.jpg","image/screenshot/Chapter9_apply.jpg","image/screenshot/Chapter12_apply.jpg"]
 
+// Anchor
+let overlayAnchor = ["https://ehammlette.github.io/Webdev24/chapter2/template.html","https://ehammlette.github.io/Webdev24/chapter3/analyze/analyze03.html","https://ehammlette.github.io/Webdev24/chapter4/analyze/analyze04.html","https://ehammlette.github.io/Webdev24/chapter5/extend/index.html","https://ehammlette.github.io/Webdev24/chapter06/extend/extend06.html","https://ehammlette.github.io/Webdev24/chapter07/analyze/analyze07.html","https://ehammlette.github.io/Webdev24/chapter09/apply/index.html"]
 // Captions associated with each image
 let imgCaptions = new Array(8);
-imgCaptions[0]="Sky Pond (Rocky Mountain National Park)";
-imgCaptions[1]="Buffalo on the Plains (South Dakota)"; 
-imgCaptions[2]="Garden of the Gods (Colorado Springs)"; 
-imgCaptions[3]="Elephant Head Wild Flower (Rocky Mountain National Park)"; 
-imgCaptions[4]="Double Rainbow (Colorado National Monument)";
-imgCaptions[5]="Moose in the Wild (Grand Lake, Colorado)";
-imgCaptions[6]="Camas Wild Flower (Rocky Mountain National Park)";
-imgCaptions[7]="Chasm Lake (Rocky Mountain National Park)";
+imgCaptions[0]="Chapter 2 Template";
+imgCaptions[1]="Chapter 3 "; 
+imgCaptions[2]="Chapter 4"; 
+imgCaptions[3]="Chapter 5"; 
+imgCaptions[4]="Chapter 6";
+imgCaptions[5]="Chapter 7";
+imgCaptions[6]="Chapter 9";
+imgCaptions[7]="Chapter 12";
 
 // Count of images in the slideshow
 let imgCount = imgFiles.length;
