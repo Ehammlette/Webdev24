88acaef216215e0b96d25314fdb283f97de7fd6e
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-05-01 12:02:00 -0500

    Update js05_copy.js

--- Final/lightbox/js05_copy.js
+++ Final/lightbox/js05_copy.js
@@ -94,6 +94,39 @@ function createLightbox(){
          (currentImg > 1) ? currentImg-- : currentImg = imgCount;
          lbCounter.textContent = currentImg +" / "+ imgCount;
       }
+
+      //function to create an overlay for the window
+      function createOverlay(){
+         let overlay= document.createElement("div");
+         overlay.id= "lbOverlay";
+
+         //Add the figure box to th overlay
+         let figureBox= document.createElement("figure");
+         overlay.appendChild(figureBox);
+
+         //Add the image to the figure box
+         let overlayImage = this.cloneNode("true");
+         figureBox.appendChild(overlayImage);
+
+         //Add the caption to the figure box
+         let overlayCaption= document.createElement("figcaption");
+         overlayCaption.textContent = this.alt;
+         figureBox.appendChild(overlayCaption);
+
+         document.body.appendChild(overlay);
+
+         //Add a close button to the overlay
+         let closeBox = document.createElement("div");
+         closeBox.id = "lbOverlayClose";
+         closeBox.innerHTML = "&times;";
+
+         closeBox.onclick = function(){
+            document.body.removeChild(overlay);
+         }
+         overlay.appendChild(closeBox);
+   
+         document.body.appendChild(overlay);
+      }
 }
 
 
