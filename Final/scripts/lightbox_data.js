bdb247ae4541bfd8886a430bacd3078d8f36261c
Ehammlette <ethan.hammons.22@student.prcc.edu>
2024-05-03 10:25:57 -0500

    Update lightbox_data.js

--- Final/scripts/lightbox_data.js
+++ Final/scripts/lightbox_data.js
@@ -15,6 +15,7 @@ let lightboxTitle = "Client side";
 let imgFiles = [,"image/screenshot/chapter2-02.jpg","image/screenshot/chapter3-02.jpg","image/screenshot/chapter4-02.jpg","image/screenshot/chapter5-02.jpg","image/screenshot/chapter6-1.jpg","image/screenshot/chapter7-01.jpg","image/screenshot/chapter9-01.jpg","image/screenshot/chapter12-3.jpg"]
 
 // Anchor
+let overlayAnchor = ["https://ehammlette.github.io/Webdev24/Client_Side/Chapter02/project02/project02-02.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter03/project02/project03-02.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter04/project02/project04-02.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter05/project02/project05-02.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter06/project01/project06-01.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter07/project01/project07-01.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter09/project01/project09-01a.html","https://ehammlette.github.io/Webdev24/Client_Side/Chapter12/project03/project12-03.html"]
 // Captions associated with each image
 let imgCaptions = new Array(8);
 imgCaptions[0]="Chapter 2-02";
